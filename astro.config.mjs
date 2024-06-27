1c65f7ea8fee84aed87ed3e2239909b8e1765477
Jason Luo <22487340+xemexpress@users.noreply.github.com>
2024-06-27 16:50:42 +0800

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3000
-  },
-  integrations: [tailwind(), svelte()],
-  site: 'https://rhythm.nation'
+    server: {
+        port: 3000,
+    },
+    integrations: [tailwind(), svelte(), sitemap()],
+    site: "https://main--peaceful-mousse-87c347.netlify.app/",
 });
